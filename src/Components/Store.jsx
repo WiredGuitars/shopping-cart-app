da90a250fd541e904d750e34e6db287c78e23766
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2023-11-11 18:20:58 -0600

    quicksave before I potentially mess up Store component

--- src/Components/Store.jsx
+++ src/Components/Store.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { fetchProductData } from "./Fetch";
-import Styles from "../assets/Preview.module.css"
-
+import Styles from "../assets/Preview.module.css";
 
 const Store = () => {
   const [products, setProducts] = useState([]);
+  const [userInput, setUserInput] = useState('')
+  const navigate = useNavigate();
+
   useEffect(() => {
     async function fetchData() {
       try {
         const data = await fetchProductData();
-        console.log(data)
         setProducts(data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -19,44 +20,70 @@ const Store = () => {
     fetchData();
   }, []);
 
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value)
+  }
+
   function handleImageClick(product) {
-    console.log(`Clicked on ${product.title}`)
-  } 
+    const productId = product.id;
+    navigate(`/Store/${productId}`, { state: { product } });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("User input:", userInput);
+  };
 
   const location = useLocation();
-  const product = location.state ? location.state.product : null;
-  console.log(product)
-  if (!product) {
-    
+  const prevInfo = location.state ? location.state.product : null;
+  if (!prevInfo) {
     return (
       <div className={Styles.itemBoxes}>
-      {products.map((product) => (
-      <div className={`${Styles.previewBoxes}`} style = {{maxWidth: 'fit-content' }} key={product.id} onClick={() => handleImageClick(product)}>
+        {products.map((product) => (
+          <div
+            className={`${Styles.previewBoxes}`}
+            style={{ maxWidth: "fit-content" }}
+            key={product.id}
+            onClick={() => handleImageClick(product)}
+          >
             <img
               className={Styles.previewImgs}
               src={product.image}
-              style={ {maxWidth: '250px',
-                      maxHeight: '150px'}}
-              alt={product.title}/>
+              style={{ maxWidth: "250px", maxHeight: "150px" }}
+              alt={product.title}
+            />
             <h1>${product.price.toFixed(2)}</h1>
           </div>
-      ))}  
+        ))}
+      </div>
+    );
+  } else if (prevInfo) {
+    return (
+      <div>
+        <div
+          className={Styles.previewBoxes}
+          onClick={() => console.log(prevInfo)}
+          style={{ width: "fit-content" }}
+          key={prevInfo.id}
+        >
+          <img
+            className={Styles.previewImgs}
+            src={prevInfo.image}
+            alt={prevInfo.title}
+          />
+          <h1>${prevInfo.price.toFixed(2)}</h1>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="How many do you want?"
+            className={Styles.inputField}
+            onChange={handleInputChange}
+          />
+        </form>
+        <button className={Styles.addToCart}>Add To Cart</button>
       </div>
     );
   }
-
-  return (
-    <div>
-      <div className={Styles.previewBoxes} onClick={() => handleImageClick(product)} style = {{width: 'fit-content'}} key={product.id}>
-      <img 
-        className={Styles.previewImgs}
-        src={product.image}
-        alt={product.title}/>
-        <h1>${product.price.toFixed(2)}</h1>
-      </div>
-      
-    </div>
-  );
 };
 
 export default Store;
